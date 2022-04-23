ec7c87c796356a954639847e2253dfdf46d16ee6
LeooZeballos <zeballosleonel3@gmail.com>
2022-04-23 00:37:17 -0300

    Validation of registration

--- src/main/java/com/leozeballos/regsys/registration/RegistrationService.java
+++ src/main/java/com/leozeballos/regsys/registration/RegistrationService.java
@@ -1,10 +1,19 @@
 package com.leozeballos.regsys.registration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalArgumentException("Invalid email");
+        }
         return "works";
     }
 }
