a847f974880c8a62112bf16cb01162a2b793d69c
LeooZeballos <zeballosleonel3@gmail.com>
2022-04-22 22:59:02 -0300

    Initial commit

---
+++ src/main/java/com/leozeballos/regsys/RegistrationSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.leozeballos.regsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSystemSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationSystemSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leozeballos/regsys/RegistrationSystemSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leozeballos.regsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSystemSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
