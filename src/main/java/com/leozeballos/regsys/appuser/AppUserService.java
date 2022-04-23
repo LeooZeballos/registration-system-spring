0e9e83b654fd354073e656b2a70ffb35c749d54d
LeooZeballos <zeballosleonel3@gmail.com>
2022-04-23 15:10:57 -0300

    removed unnecessary todo

--- src/main/java/com/leozeballos/regsys/appuser/AppUserService.java
+++ src/main/java/com/leozeballos/regsys/appuser/AppUserService.java
@@ -53,8 +53,6 @@ public class AppUserService implements UserDetailsService {
 
         confirmationTokenService.saveConfirmationToken(confirmationToken);
 
-        // TODO: Send email
-
         return token;
     }
 
